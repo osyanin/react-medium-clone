40511b3c66d8bbb2dfb9900102a7f365c3ea91bd
Boris <osyanin(osyanin@gmail.com)>
2020-01-02 23:17:53 +0300

    Almost done thing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-import Routes from 'pages/routes';
-import TopBar from 'components/topBar';
+import Routes from 'routes'
+import Topbar from 'components/topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
-    return (
-        <div>
-            <Router>
-                <TopBar/>
-                <Routes/>
-            </Router>
-        </div>
-    )
-};
+  return (
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+        <Router>
+          <Topbar />
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
